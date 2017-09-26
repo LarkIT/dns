d9e42d8001b7d7bdb6372bdb17a4094ae920cbbb
Nicolas Fosdick <nick.fosdick@sigfig.com>
2017-09-26 14:34:53 -0600

    exporting external route52 id

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,7 @@ output "external_dns_enable" {
 output "route53_internal_id" {
   value = "${aws_route53_zone.internal.id}"
 }
+
+output "route53_external_id" {
+  value = "${aws_route53_zone.external.id}"
+}
